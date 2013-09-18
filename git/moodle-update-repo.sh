e140df4a85ab0257b4d6e45bf70c2b596d173e9f
PJ King <pking@sebts.edu>
2013-09-18 09:56:00 -0400

    restructured gitlib et al. files

--- git/moodle-update-repo.sh
+++ git/moodle-update-repo.sh
@@ -9,42 +9,42 @@ do
 done
 
 # moodle-theme_zebra
-git-merge.sh 	-t $t/moodle-theme_zebra \
+gitlib-merge 	-t $t/moodle-theme_zebra \
 				-u https://github.com/thedannywahl/Zebra_4_Moodle_2.git	\
 				-o https://github.com/sebts/moodle-theme_zebra.git
 
-git-merge.sh	-t $t/moodle-theme_zebra \
+gitlib-merge	-t $t/moodle-theme_zebra \
 				-b master \
 				-d SEBTS
 
 # moodle-mod_turnitintool
-git-merge.sh 	-t $t/moodle-mod_turnitintool \
+gitlib-merge 	-t $t/moodle-mod_turnitintool \
 				-u https://github.com/ip-pauldawson/MoodleDirect.git \
 				-o https://github.com/sebts/moodle-mod_turnitintool.git
 
-git-merge.sh	-t $t/moodle-mod_turnitintool \
+gitlib-merge	-t $t/moodle-mod_turnitintool \
 				-b master \
 				-d release
 				
 # moodle-mod_questionnaire
-git-merge.sh 	-t $t/moodle-mod_questionnaire \
+gitlib-merge 	-t $t/moodle-mod_questionnaire \
 				-u https://github.com/remotelearner/moodle-mod_questionnaire.git \
 				-b "MOODLE_23_STABLE MOODLE_24_STABLE MOODLE_25_STABLE" \
 				-o https://github.com/sebts/moodle-mod_questionnaire.git 
 
-git-merge.sh	-t $t/moodle-mod_questionnaire \
+gitlib-merge	-t $t/moodle-mod_questionnaire \
 				-b MOODLE_23_STABLE \
 				-d SEBTS_23
 
 # moodle
-git-merge.sh	-t $t/moodle \
+gitlib-merge	-t $t/moodle \
 				-u https://github.com/moodle/moodle.git	\
 				-b "MOODLE_23_STABLE MOODLE_24_STABLE MOODLE_25_STABLE" \
 				-o https://github.com/sebts/moodle.git
 
-git-merge.sh	-t $t/moodle \
+gitlib-merge	-t $t/moodle \
 				-b MOODLE_23_STABLE \
 				-d SEBTS_23
 				
-git-submodule.sh	-t $t/moodle \
+gitlib-submodule	-t $t/moodle \
 					-b SEBTS_23
