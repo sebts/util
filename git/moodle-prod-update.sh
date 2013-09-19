d2eda8d467cd2767a97432b99090aab423cdcc60
PJ King <pking@sebts.edu>
2013-09-19 09:03:27 -0400

    gitlib-update added

---
+++ git/moodle-prod-update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gitlib-update       -t /c/@/test/web \
+                    -d /c/@/test/git \
+                    -u https://github.com/sebts/moodle.git \
+                    -b SEBTS_RELEASE
