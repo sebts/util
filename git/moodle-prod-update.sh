3f3dd0a72e7635ae3b1ccd27a58e6598f1ea65e9
PJ King <pking@sebts.edu>
2013-10-08 08:07:24 -0400

    call bash directly

--- git/moodle-prod-update.sh
+++ git/moodle-prod-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gitlib-update       -t /usr/share/moodle \
+bash gitlib-update  -t /usr/share/moodle \
                     -d /var/lib/moodle.git \
                     -u https://github.com/sebts/moodle.git \
                     -b SEBTS_RELEASE
--- git/moodle-repo-update.sh
+++ git/moodle-repo-update.sh
@@ -9,42 +9,42 @@ do
 done
 
 # moodle-theme_zebra
-gitlib-merge    -t $t/moodle-theme_zebra \
-                -u https://github.com/thedannywahl/Zebra_4_Moodle_2.git    \
-                -o https://github.com/sebts/moodle-theme_zebra.git
+bash gitlib-merge -t $t/moodle-theme_zebra \
+                  -u https://github.com/thedannywahl/Zebra_4_Moodle_2.git \
+                  -o https://github.com/sebts/moodle-theme_zebra.git
 
-gitlib-merge    -t $t/moodle-theme_zebra \
-                -b master \
-                -d SEBTS
+bash gitlib-merge -t $t/moodle-theme_zebra \
+                  -b master \
+                  -d SEBTS
 
 # moodle-mod_turnitintool
-gitlib-merge    -t $t/moodle-mod_turnitintool \
-                -u https://github.com/ip-pauldawson/MoodleDirect.git \
-                -o https://github.com/sebts/moodle-mod_turnitintool.git
+bash gitlib-merge -t $t/moodle-mod_turnitintool \
+                  -u https://github.com/ip-pauldawson/MoodleDirect.git \
+                  -o https://github.com/sebts/moodle-mod_turnitintool.git
 
-gitlib-merge    -t $t/moodle-mod_turnitintool \
-                -b master \
-                -d release
-                
+bash gitlib-merge -t $t/moodle-mod_turnitintool \
+                  -b master \
+                  -d release
+
 # moodle-mod_questionnaire
-gitlib-merge    -t $t/moodle-mod_questionnaire \
-                -u https://github.com/remotelearner/moodle-mod_questionnaire.git \
-                -b "MOODLE_23_STABLE MOODLE_24_STABLE MOODLE_25_STABLE" \
-                -o https://github.com/sebts/moodle-mod_questionnaire.git 
+bash gitlib-merge -t $t/moodle-mod_questionnaire \
+                  -u https://github.com/remotelearner/moodle-mod_questionnaire.git \
+                  -b "MOODLE_23_STABLE MOODLE_24_STABLE MOODLE_25_STABLE" \
+                  -o https://github.com/sebts/moodle-mod_questionnaire.git 
 
-gitlib-merge    -t $t/moodle-mod_questionnaire \
-                -b MOODLE_23_STABLE \
-                -d SEBTS_23
+bash gitlib-merge -t $t/moodle-mod_questionnaire \
+                  -b MOODLE_23_STABLE \
+                  -d SEBTS_23
 
 # moodle
-gitlib-merge    -t $t/moodle \
-                -u https://github.com/moodle/moodle.git    \
-                -b "MOODLE_23_STABLE MOODLE_24_STABLE MOODLE_25_STABLE" \
-                -o https://github.com/sebts/moodle.git
+bash gitlib-merge -t $t/moodle \
+                  -u https://github.com/moodle/moodle.git    \
+                  -b "MOODLE_23_STABLE MOODLE_24_STABLE MOODLE_25_STABLE" \
+                  -o https://github.com/sebts/moodle.git
 
-gitlib-merge    -t $t/moodle \
-                -b MOODLE_23_STABLE \
-                -d SEBTS_23
-                
-gitlib-submodule    -t $t/moodle \
-                    -b SEBTS_23
+bash gitlib-merge -t $t/moodle \
+                  -b MOODLE_23_STABLE \
+                  -d SEBTS_23
+
+bash gitlib-submodule -t $t/moodle \
+                      -b SEBTS_23
