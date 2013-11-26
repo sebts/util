4f4fe6b86544b51a52befbfdea0d31c57e094057
root <root@moodle2012.campus.sebts.edu>
2013-11-26 00:44:13 -0500

    going to SEBTS_25'

--- git/moodle-prod-update.sh
+++ git/moodle-prod-update.sh
@@ -3,4 +3,4 @@
 bash gitlib-update  -t /usr/share/moodle \
                     -d /var/lib/moodle.git \
                     -u https://github.com/sebts/moodle.git \
-                    -b SEBTS_RELEASE
+                    -b SEBTS_25
