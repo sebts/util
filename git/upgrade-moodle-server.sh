f66a51496541b0dd91f782243625dde575d7329c
PJ King <pking@sebts.edu>
2014-02-06 11:51:21 -0500

    added -m switch
    `-m true` puts moodle in maintenance mode. `-m false` does a git update
    without upgrading moodle.

--- git/upgrade-moodle-server.sh
+++ git/upgrade-moodle-server.sh
@@ -4,8 +4,9 @@ b= #branch name
 t="/usr/share/moodle" #git working tree. also the production directory
 d="/var/lib/moodle.git" #git repository directory.
 u="https://github.com/sebts/moodle.git" #repository url
+m=true
 w=0
-i=true
+i=false
 s="www-data"
 
 usage()
@@ -17,26 +18,28 @@ This script will update a moodle installation via using git.
 
 OPTIONS:
   
-  $0 -b <branch> [-t <path>] [-d <path>] [-u <url>] [-w <int>] [-i <bool>] [-s <user>]
+  $0 -b <branch> [-t <path>] [-d <path>] [-u <url>] [-m <bool> [-w <int>] [-i <bool>] [-s <user>]
 
   -b Sets the branch name to pull from git.
   -t Sets the work tree, i.e., production directory. Default: "/usr/share/moodle"
   -d Sets the git directory. Default: "/var/lib/moodle.git"
   -u Sets the repository url. Default: "https://github.com/sebts/moodle.git"
+  -m Sets moodle maintenance mode. Default: true
   -w Sets the number of minutes to warn moodle users before running update. Default: 0
-  -i Sets interactive mode for moodle upgrade. Default: true
+  -i Sets interactive mode for moodle upgrade. Default: false
   -s Sets su for moodle cli. Default: "www-data"
 EOF
 }
 
 # begin arguments
-while getopts "b:t:d:u:w:i:s:" OPTION
+while getopts "b:t:d:u:m:w:i:s:" OPTION
 do
     case $OPTION in
         b) b=$OPTARG ;;
         t) t=$OPTARG ;;
         d) d=$OPTARG ;;
         u) u=$OPTARG ;;
+        m) m=$OPTARG ;;
         w) w=$OPTARG ;;
         i) i=$OPTARG ;;
         s) s=$OPTARG ;;
@@ -44,13 +47,13 @@ do
      esac
 done
 
-if [[ -z $t ]] || [[ -z $d ]] || [[ -z $u ]] || [[ -z $b ]] || [[ -z $s ]]
+if [[ -z $b ]] || [[ -z $t ]] || [[ -z $d ]] || [[ -z $u ]] || [[ -z $m ]] || [[ -z $w ]] || [[ -z $i ]] || [[ -z $s ]]
 then
     usage ; exit 1;
 fi
 # end arguments
 
-if  [[ -d $t ]] # if moodle exists, put it in maintenance mode
+if [[ $m ]] && [[ -d $t ]] # if moodle exists, put it in maintenance mode
 then
 	# Moodle maintenance mode
 	if [[ $w > 0 ]]
@@ -66,9 +69,14 @@ fi
 # Update from git
 bash gitlib-update  -t $t -d $d -u $u -b $b
 
-# Upgrade moodle database
+# Always upgrade moodle database. Even if we didn't put moodle in maintenance mode
+# it's better to run the upgrade script while moodle is running than to accidentally *need* to run it, but fail to.
 [[ $i ]] || uparg="--non-interactive"
 sudo -u $s /usr/bin/php $t/admin/cli/upgrade.php $uparg
 
-# All done
-sudo -u $s /usr/bin/php $t/admin/cli/maintenance.php --disable # re-open
+
+if [[ $m ]]
+then
+	# All done
+	sudo -u $s /usr/bin/php $t/admin/cli/maintenance.php --disable # re-open
+fi
